602344191fb96430cc766a7dc34ed12da64ffc02
Adrian Munoz perez <amunoz-p@c1r13s2.42madrid.com>
2021-09-14 13:41:09 +0200

    .

--- Vector.hpp
+++ Vector.hpp
@@ -6,13 +6,22 @@
 /*   By: amunoz-p <amunoz-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 11:20:37 by amunoz-p          #+#    #+#             */
-/*   Updated: 2021/09/14 11:29:07 by amunoz-p         ###   ########.fr       */
+/*   Updated: 2021/09/14 13:22:47 by amunoz-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 
+#include "VectorIterator.hpp"
 
+template<typename T, class Alloc = allocator<T> >
+
+class Vector
+{
+    public:
+
+    
+}
 
 #endif
